e30dbda604d5cb428258dd4b4a5e540338de4e1e
Mak Khan <arham@alinon.tech>
2025-04-23 12:09:40 +0100

    added vite

--- sip_call_center_assistant/frontend/vite.config.ts
+++ sip_call_center_assistant/frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'caller-frontend-959273450812.europe-west2.run.app',
+    ],
+  },
 })
